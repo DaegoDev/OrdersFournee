225fb5d05ec32bf5a616e6958bd1d535ab8dbead
Diego Alvarez <daegodev@gmail.com>
2017-04-17 23:59:06 -0500

    fixed order creation

--- assets/js/components/controllers/orderCreateCtrl.js
+++ assets/js/components/controllers/orderCreateCtrl.js
@@ -4,25 +4,34 @@
 
   // Timepicker para el rango de hora sugerida
   function orderCreateCtrl($scope, $log, $state, $ngConfirm, ClientSvc) {
-    $scope.timeInitial = new Date();
-    $scope.timeInitial.setHours(12);
-    $scope.timeInitial.setMinutes(0);
-    $scope.timeFinal = $scope.timeInitial;
+    $scope.order = {};
+    $scope.order.timeInitial = new Date();
+    $scope.order.timeInitial.setHours(12);
+    $scope.order.timeInitial.setMinutes(0);
+    $scope.order.timeFinal = $scope.order.timeInitial;
 
-    $scope.hstep = 1;
-    $scope.mstep = 10;
-    $scope.minTimeFinal = $scope.timeInitial;
-    $scope.minTimeInitial = new Date();
-    $scope.minTimeInitial.setHours(9);
-    $scope.minTimeInitial.setMinutes(0);
-    $scope.maxTimeInitial = new Date();
-    $scope.maxTimeInitial.setHours(18);
-    $scope.maxTimeInitial.setMinutes(0);
-    $scope.maxTimeFinal = $scope.maxTimeInitial;
+    $scope.order.hstep = 1;
+    $scope.order.mstep = 10;
+    $scope.order.minTimeFinal = $scope.order.timeInitial;
+    $scope.order.minTimeInitial = new Date();
+    $scope.order.minTimeInitial.setHours(9);
+    $scope.order.minTimeInitial.setMinutes(0);
+    $scope.order.maxTimeInitial = new Date();
+    $scope.order.maxTimeInitial.setHours(18);
+    $scope.order.maxTimeInitial.setMinutes(0);
+    $scope.order.maxTimeFinal = $scope.order.maxTimeInitial;
 
     // Variables para el control de la lista de productos.
-    $scope.order=  {};
     $scope.orderList = [];
+
+    // Dropdown para listar los empleados del cliente
+    ClientSvc.getClientEmployees()
+      .then(function(res) {
+        $scope.placement = {
+          options: res.data,
+          selected: res.data[0],
+        };
+      });
 
     ClientSvc.getProductsClient()
       .then(function(res) {
@@ -36,6 +45,12 @@
       $scope.makeOrder = function () {
         var productsToOrder = [];
         var product = null;
+        var tmpDeliveryDate = null;
+        var tmpClientEmployee = null;
+        var tmpInitialTime = null;
+        var tmpFinalTime = null;
+        var tmpInformation = null;
+
         $scope.orderList.forEach(function (product) {
           product = {
             client_product: product.client_product,
@@ -44,13 +59,30 @@
           }
           productsToOrder.push(product);
         });
+
+        tmpDeliveryDate = $scope.order.dt.getFullYear() + '-' + $scope.order.dt.getMonth() + '-' + $scope.order.dt.getDate();
+        tmpClientEmployee = $scope.placement.selected.id;
+
+        if ($scope.order.timeInitial.getMinutes() <= 9) {
+          tmpInitialTime = $scope.order.timeInitial.getHours() + ':0' + $scope.order.timeInitial.getMinutes();
+        } else {
+          tmpInitialTime = $scope.order.timeInitial.getHours() + ':' + $scope.order.timeInitial.getMinutes();
+        }
+
+        if ($scope.order.timeFinal.getMinutes() <= 9) {
+          tmpFinalTime = $scope.order.timeFinal.getHours() + ':0' + $scope.order.timeFinal.getMinutes();
+        } else {
+          tmpFinalTime = $scope.order.timeFinal.getHours() + ':' + $scope.order.timeFinal.getMinutes();
+        }
+
+        tmpInformation = $scope.order.additionalInformation;
+
         var orderCredentials = {
-          dt: $scope.dt,
-          deliveryDate: $scope.dt.getFullYear() + '-' + $scope.dt.getMonth() + '-' + $scope.dt.getDate(),
-          clientEmployee: $scope.placement.selected.id,
-          initialSuggestedTime: $scope.timeInitial.getHours() + ':' + $scope.timeInitial.getMinutes(),
-          finalSuggestedTime: $scope.timeFinal.getHours()+ ':' + $scope.timeFinal.getMinutes(),
-          additionalInformation: $scope.order.additionalInformation,
+          deliveryDate: tmpDeliveryDate,
+          clientEmployee: tmpClientEmployee,
+          initialSuggestedTime: tmpInitialTime,
+          finalSuggestedTime: tmpFinalTime,
+          additionalInformation: tmpInformation,
           productsToOrder: productsToOrder
         }
         ClientSvc.makeOrder(orderCredentials)
@@ -79,6 +111,7 @@
             $log.info(res.data);
           })
           .catch(function (err) {
+            $ngConfirm('El pedido no ha sido creado, verifique la fecha de entrega.');
             $log.error(err);
           });
       }
@@ -99,8 +132,8 @@
 
     $scope.changedInitial = function() {
       // $log.log('Time changed to: ' + $scope.timeInitial);
-      $scope.timeFinal = $scope.timeInitial;
-      $scope.minTimeFinal = $scope.timeInitial;
+      $scope.order.timeFinal = $scope.order.timeInitial;
+      $scope.order.minTimeFinal = $scope.order.timeInitial;
     };
 
     $scope.changedFinal = function() {
@@ -109,7 +142,7 @@
 
     // Datepicker para la fecha de entrega
     $scope.today = function() {
-      $scope.dt = new Date();
+      $scope.order.dt = new Date();
     };
     $scope.today();
 
@@ -139,7 +172,7 @@
     };
 
     $scope.setDate = function(year, month, day) {
-      $scope.dt = new Date(year, month, day);
+      $scope.order.dt = new Date(year, month, day);
     };
 
     $scope.popup1 = {
@@ -176,14 +209,5 @@
       }
       return '';
     }
-
-    // Dropdown para listar los empleados del cliente
-    ClientSvc.getClientEmployees()
-      .then(function(res) {
-        $scope.placement = {
-          options: res.data,
-          selected: res.data[0],
-        };
-      });
   }
 }())

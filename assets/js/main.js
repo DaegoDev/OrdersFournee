cee03a8cdca5af8b2e1e30414fdfc478be086896
Jonnatan <jrios328@gmail.com>
2017-08-26 19:31:42 -0500

    Function print command

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 'use stric';
 var fournee = angular.module('fournee', ['ui.router', 'permission', 'permission.ui', 'ngMessages', 'ngPassword',
-  'ngAnimate', 'ui.bootstrap', 'ngCookies', 'cp.ngConfirm', '720kb.tooltips', 'summernote', 'ngSanitize'
+  'ngAnimate', 'ui.bootstrap', 'ngCookies', 'cp.ngConfirm', '720kb.tooltips', 'summernote', 'ngSanitize', 'AngularPrint'
 ]);
 
 // Inicializacion de la configuracion principal al ingresar al dominio.
